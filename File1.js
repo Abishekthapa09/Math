7083af64691921c92717071fddc5263769a9a235
Abishek Thapa <109749100+Abishekthapa09@users.noreply.github.com>
2023-06-25 14:56:51 +0545

    ok

---
+++ File1.js
@@ -0,0 +1,9 @@
+
+const Myname=()=>{
+    console.log("Abishek Thapa")
+}
+Myname()
+
+//Create a function to sort an Array in an ascending order
+const Num = [40, 100, 58, 3, 25, 10];
+Num.sort(function(a, b){return a-b});
