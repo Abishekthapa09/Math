37b4619f3e0c0e2a68cc5a9fca25b9fa887f3b50
Ram Gurung <ramgrg1024@gmail.com>
2023-06-25 15:11:35 +0545

    descending order done

--- File1.js
+++ File1.js
@@ -7,4 +7,5 @@ Myname()
 //Create a function to sort an Array in an ascending order
 let numArray = [3,34, 14, 16, 79, 2];
 let sortedArr = numArray.sort();
+sortedArr = sortedArr.reverse();
 console.log(sortedArr);
